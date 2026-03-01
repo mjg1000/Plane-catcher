39b686f9d9449f33b9f6f764652b98302289b9f7
ElliotWride <elliotwride@gmail.com>
2026-03-01 10:44:29 +0000

    internal

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,14 +1,32 @@
 import { bottomNavStyle, navButton } from "../styles/navStyles";
+
 interface BottomNavProps {
-  setPage: (page: "map" | "inventory" | "rewards") => void;
+  setPage: (page: "map" | "inventory" | "rewards" | "internalMap") => void;
+  currentPage: "map" | "inventory" | "rewards" | "internalMap";
 }
 
-export default function BottomNav({ setPage }: BottomNavProps) {
+export default function BottomNav({ setPage, currentPage }: BottomNavProps) {
+  const handleMapClick = () => {
+    if (currentPage === "map") {
+      // Switch to internal map
+      setPage("internalMap");
+    } else {
+      // Go to map page
+      setPage("map");
+    }
+  };
+
   return (
     <div style={bottomNavStyle}>
-      <button style={navButton} onClick={() => setPage("map")}>🗺 Map</button>
-      <button style={navButton} onClick={() => setPage("inventory")}>🎒 Inventory</button>
-      <button style={navButton} onClick={() => setPage("rewards")}>🏆 Rewards</button>
+      <button style={navButton} onClick={handleMapClick}>
+        🗺 Map
+      </button>
+      <button style={navButton} onClick={() => setPage("inventory")}>
+        🎒 Inventory
+      </button>
+      <button style={navButton} onClick={() => setPage("rewards")}>
+        🏆 Rewards
+      </button>
     </div>
   );
 }
